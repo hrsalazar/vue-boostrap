44eb4527b124df824ecf8e211c51f2703274fa1d
hrsalazar <hector.salazar1@tafensw.edu.au>
2019-04-16 13:57:31 +1000

    Iniital commit

---
+++ src/worklets/circle.js
@@ -0,0 +1,9 @@
+class CirclePainter {
+    paint(ctx, geom, properties) {
+      ctx.beginPath();
+      ctx.fillStyle = 'red';
+      ctx.arc(geom.height/2, geom.width/2 ,25 ,0 , 2*Math.PI);
+      ctx.fill();
+    }
+  }
+  registerPaint('circle', CirclePainter);
