ba165ab9e3d3d5a074d46858849dfcde8f1da673
hrsalazar <hector.salazar1@tafensw.edu.au>
2019-06-06 16:24:18 +1000

    Fetching data from API using axios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <div id="app">
+    <h1>Bitcoin Price Index</h1>
+
+  <section v-if="errored">
+    <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+  </section>
+
+  <section v-else>
+    <div v-if="loading">Loading...</div>
+
+    <div
+      v-else
+      v-for="currency in info"
+      class="currency"
+    >
+      {{ currency.description }}:
+      <span class="lighten">
+        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+      </span>
+    </div>
+
+  </section>
     <navigation />
     <router-view/>
   </div>
@@ -7,11 +28,37 @@
 
 <script>
 import navigation from './components/Navigation';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 export default {
   name: 'App',
   components: {
     navigation,
+  },
+  data() {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  filters: {
+    currencydecimal (value) {
+      return value.toFixed(2)
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((response) => {
+        this.info = response.data.bpi;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => { this.loading = false; });
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-console */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App';
@@ -11,7 +14,7 @@ import router from './router';
 
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue, VueAxios, axios);
 
 /* eslint-disable no-new */
 new Vue({
